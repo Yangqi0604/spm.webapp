223c2c5259935cacff6aeb673146c6aa5fb7ac6f
YangAaron <jianfeng_1511@163.com>
2020-06-02 09:24:24 +0800

    bugfix

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    [
-      '@vue/app',
-      {
-        debug: true,
-        polyfills: [ 'es6.promise', 'es6.array.find-index', 'es7.array.includes', 'es6.string.includes' ]
-      }
-    ]
+    '@vue/cli-plugin-babel/preset'
   ]
 }
